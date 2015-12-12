81613385d92d8e84e08541f1853c0a7bbbdecfbd
lizhizhou <lzz@meteroi.com>
2015-12-12 05:14:44 -0800

    with bug

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bigdata
 Big data program for FPGAduino
+Inlcude hadoop
+Nodejs map reduce
--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-var Parallel = require('Parallel');
+var Parallel = require('paralleljs/lib/paralleljs');
+var dnode    = require('dnode');
 
-var p = new Parallel([1, 2, 3, 4, 5]);
+var server = dnode(function (remote, conn) {
+    this.zing = function (n, cb) { cb(n * 100) };
+});
+server.listen(7070);
+
+//var p = new Parallel([1, 2, 3, 4, 5]);
  
-console.log(p.data); // prints [1, 2, 3, 4, 5] 
+//console.log(p.data); // prints [1, 2, 3, 4, 5] 
